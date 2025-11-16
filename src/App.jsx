36e2217a4c409317a98f6026f7899bcda8834d6b
vedanti <vedanti.r.bagade@gmail.com>
2025-11-16 11:05:19 +0530

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
+import Category from "./pages/Category";
+import Filter from "./pages/Filter";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
-  return <div className="text-3xl font-bold underline">Xpenso</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Home />}></Route>
+        <Route path="/income" element={<Income />}></Route>
+        <Route path="/expense" element={<Expense />}></Route>
+        <Route path="/category" element={<Category />}></Route>
+        <Route path="/filter" element={<Filter />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Expense.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Expense() {
+  return <div>Expense</div>;
+}
+
+export default Expense;
