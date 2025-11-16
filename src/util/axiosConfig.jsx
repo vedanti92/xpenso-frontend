f2c445b78c19481df748df6f12ea8722d595e383
vedanti <vedanti.r.bagade@gmail.com>
2025-11-16 17:37:29 +0530

    file updated

---
+++ src/util/axiosConfig.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const axiosConfig = axios.create({
+  baseURL: "https://xpenso-178l.onrender.com/api/v1.0",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// list of endpoints that do not require authorization header
+const excludeEndpoints = [
+  "/login",
+  "/register",
+  "/status",
+  "/activate",
+  "/health",
+];
+
+// request interceptor
+axiosConfig.interceptors.request.use(
+  (config) => {
+    const shouldSkipToken = excludeEndpoints.some((endpoint) => {
+      config.url.includes(endpoint);
+    });
+
+    if (!shouldSkipToken) {
+      const accessToken = localStorage.getItem("token");
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor
+axiosConfig.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again later.");
+    }
+    return Promise.reject(error);
+  }
+);
