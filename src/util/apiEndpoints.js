ce8ffab430e98f1f928870979e65931f69751fd0
vedanti <vedanti.r.bagade@gmail.com>
2025-11-18 17:37:56 +0530

    file updated

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,6 +1,9 @@
 export const BASE_URL = "https://xpenso-178l.onrender.com/api/v1.0";
 
+const CLOUDINARY_CLOUD_NAME = dui1ebto3;
+
 export const API_ENDPOINTS = {
   LOGIN: "/login",
   REGISTER: "/register",
+  UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
