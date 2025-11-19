aee59c14d812de13c74981fa5876f2ee9b3ddb1e
vedanti <vedanti.r.bagade@gmail.com>
2025-11-19 18:20:42 +0530

    file updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Menubar from "./Menubar";
+import { AppContext } from "../context/AppContext";
 
 function Dashboard() {
+  const { user } = useContext(AppContext);
   return (
     <div>
       <Menubar />
+      {user && (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            {/* Side bar content */}
+            <div>Sidebar content</div>
+          </div>
+          <div className="grow mx-5">Right side content</div>
+        </div>
+      )}
     </div>
   );
 }
