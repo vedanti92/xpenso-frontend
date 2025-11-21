229adba742275bd8f4c05b6d7094c7b91140fef9
vedanti <vedanti.r.bagade@gmail.com>
2025-11-21 17:02:52 +0530

    file updated

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Dashboard from "../components/Dashboard";
 import { useUser } from "../hooks/useUser";
+import { Plus } from "lucide-react";
 
 function Category() {
   useUser();
 
   return (
     <div>
-      <Dashboard activeMenu="Category">This is category page</Dashboard>
+      <Dashboard activeMenu="Category">
+        <div className="my-5 mx-auto">
+          {/* Button to add category list */}
+          <div className="flex justify-between items-center mb-5">
+            <h2 className="text-2xl font-semibold">All Categories</h2>
+            <button className="add-btn flex items-center gap-1">
+              <Plus size={15} /> Add Category
+            </button>
+          </div>
+
+          {/* Category list */}
+
+          {/* Add category model */}
+
+          {/* Update category model */}
+        </div>
+      </Dashboard>
     </div>
   );
 }
