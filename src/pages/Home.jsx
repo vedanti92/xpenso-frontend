7147a3830c6f3b9be2867b5085613ed2828685a2
vedanti <vedanti.r.bagade@gmail.com>
2025-11-24 11:38:31 +0530

    file updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dashboard from "../components/Dashboard";
 import { useUser } from "../hooks/useUser";
+import InfoCard from "../components/InfoCard";
+import { Coins, Wallet, WalletCards } from "lucide-react";
+import { addThousandsSeparator } from "../util/util";
+import { useNavigate } from "react-router-dom";
+import axiosConfig from "../util/axiosConfig";
+import { API_ENDPOINTS } from "../util/apiEndpoints";
+import toast from "react-hot-toast";
 
 function Home() {
+  const navigate = useNavigate();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosConfig.get(API_ENDPOINTS.DASHBOARD_DATA);
+      if (response.status === 200) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching dashboard data: ", error);
+      toast.error("Failed to load dashboard data.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   useUser();
-  
+
   return (
     <div>
-      <Dashboard activeMenu="Dashboard">This is home page</Dashboard>
+      <Dashboard activeMenu="Dashboard">
+        <div className="my-5 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* Cards */}
+            <InfoCard
+              icon={<WalletCards />}
+              label="Total Balance"
+              value={addThousandsSeparator(dashboardData.totalBalance || 0)}
+              color="bg-blue-500"
+            />
+            <InfoCard
+              icon={<Wallet />}
+              label="Total Income"
+              value={addThousandsSeparator(dashboardData.totalIncome || 0)}
+              color="bg-green-500"
+            />
+            <InfoCard
+              icon={<Coins />}
+              label="Total Expense"
+              value={addThousandsSeparator(dashboardData.totalExpense || 0)}
+              color="bg-red-500"
+            />
+          </div>
+          <div className="grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+            {/* Recent transactions */}
+
+            {/* Finance overview chart */}
+
+            {/* Expense transactions */}
+
+            {/* Income transactions */}
+          </div>
+        </div>
+      </Dashboard>
     </div>
   );
 }
--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -22,5 +22,6 @@ export const API_ENDPOINTS = {
   EMAIL_INCOME: "/email/income_excel",
   EMAIL_EXPENSE: "/email/expense_excel",
   APPLY_FILTER: "/filter",
+  DASHBOARD_DATA: "/dashboard",
   UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
